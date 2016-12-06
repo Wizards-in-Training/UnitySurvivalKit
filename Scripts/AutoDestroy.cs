13c1dba0d19f347c042bd2a41329e321c937e967
Ryan <ryanleland@icloud.com>
2016-12-05 21:23:18 -0700

    More reusable scripts.

---
+++ Scripts/AutoDestroy.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class AutoDestroy : MonoBehaviour {
+
+    public float lifespan = 1f;
+
+	void Start () {
+        // Auto destroy after a given time
+        Destroy(gameObject, lifespan);
+    }
+}
---
+++ Scripts/RandomSpawn.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class RandomSpawn : MonoBehaviour {
+
+    public GameObject spawnPrefab;
+    public float rate = 10f;
+    public float chance = 75f;
+
+    private float min = 0f;
+    private float max = 100f;
+
+    void Start() {
+        InvokeRepeating("CheckSpawn", 0f, rate);
+    }
+
+    void CheckSpawn() {
+        // "roll" for the chance to spawn
+        if (Random.Range(min, max) >= chance) {
+            // Spawn the prefab
+            Instantiate(spawnPrefab, transform.position, transform.rotation);
+        }
+    }
+}
