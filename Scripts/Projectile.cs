ce3c7ecf63df43fa5c04eeb1ed7bae2f5e45dada
Ryan <ryanleland@icloud.com>
2016-12-11 20:26:37 -0700

    Would rather trigger firing projectile from player controller, etc.

--- Scripts/Projectile.cs
+++ Scripts/Projectile.cs
@@ -7,21 +7,15 @@ public class Projectile : MonoBehaviour {
 
     // Determines the location that the projectile will be instantiated.
     public GameObject spawnPosition;
-    
+
     // The projectile prefab.
     public GameObject spawnPrefab;
-    
+
     // How much for to apply to the RigidBody
     public float fireForce = 500f;
 
     // The sound clip to play when firing the projectile
     public AudioClip fireSound;
-
-    void Update() {
-        if (Input.GetButtonDown("Fire1")) {
-            Fire();
-        }
-    }
 
     void Fire() {
         // Instantiate the prefab, and fire the projectile by calling AddFOrce on the RigidBody
