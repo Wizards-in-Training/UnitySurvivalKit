1ca8aa969e06237718ba2a831a31f7cedf62f818
Ryan <ryanleland@icloud.com>
2016-12-11 20:24:10 -0700

    Adds a new projectile controller.

---
+++ Scripts/Projectile.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Projectile : MonoBehaviour {
+
+    // Determines the location that the projectile will be instantiated.
+    public GameObject spawnPosition;
+    
+    // The projectile prefab.
+    public GameObject spawnPrefab;
+    
+    // How much for to apply to the RigidBody
+    public float fireForce = 500f;
+
+    // The sound clip to play when firing the projectile
+    public AudioClip fireSound;
+
+    void Update() {
+        if (Input.GetButtonDown("Fire1")) {
+            Fire();
+        }
+    }
+
+    void Fire() {
+        // Instantiate the prefab, and fire the projectile by calling AddFOrce on the RigidBody
+        GameObject projectile = (GameObject)Instantiate(spawnPrefab, spawnPosition.transform.position, spawnPosition.transform.rotation);
+        projectile.GetComponent<Rigidbody>().AddForce(spawnPosition.transform.forward * fireForce);
+
+        // Play the fire sound effect.
+        GetComponent<AudioSource>().PlayOneShot(fireSound);
+    }
+}
