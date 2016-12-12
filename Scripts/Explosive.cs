2a298eb7cc97ff032e78148e4bde00ce0a5a32bf
Ryan <ryanleland@icloud.com>
2016-12-11 21:52:43 -0700

    Adds explosive script.

---
+++ Scripts/Explosive.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Explosive : MonoBehaviour {
+
+    // A common pattern would be to combine particle effect, and audio in the explosion prefab.
+    public GameObject explosionPrefab;
+
+    void OnCollisionEnter() {
+        // Destroy the explosive when it collides with any other rigid body.
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
